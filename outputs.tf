78590b2e491146f9fde00e57a7b6dfb72153d339
APKAYRVWGRRJSRN7KMB3 <jackie@snowball.localdomain>
2025-09-08 09:17:32 -0400

    remove an output

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,3 @@ output "vpc_id" { value = module.networking.vpc_id }
 output "azs_used" { value = module.networking.azs_used }
 output "private_subnet" { value = module.networking.private_subnet_ids }
 output "ssm_instance_profile_name" { value = aws_iam_instance_profile.jmccoy_ssm_profile.name }
-output "ssm_role_name" { value = aws_iam_role.jmccoy_ssm_role.name }
